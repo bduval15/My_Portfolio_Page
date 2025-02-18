feb939efcf2b6fe21472589a44931b1a51f7fe11
Braeden <bduval3@my.bcit.ca>
2025-02-17 21:03:55 -0800

    Email

--- api/send-email.js
+++ api/send-email.js
@@ -10,7 +10,7 @@ export default async function handler(req, res) {
   
   try {
     let transporter = nodemailer.createTransport({
-      host: "smtp.office365.com",
+      host: "smtp-mail.outlook.com",
       port: 587,
       secure: false,
       auth: {
